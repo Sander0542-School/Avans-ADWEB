e473a75019fedc53f2b649cdeda25c8df46a22e8
Sander Jochems <sanderjochems@hotmail.nl>
2022-04-25 15:11:08 +0200

    Setup angular/fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'avans-adweb-sander-tommy',
+    appId: '1:362779009246:web:4b8a3776321a6aa916624f',
+    storageBucket: 'avans-adweb-sander-tommy.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCs4aVNYV7_Ax9vB57kxZ0-_xsiSzhPvQA',
+    authDomain: 'avans-adweb-sander-tommy.firebaseapp.com',
+    messagingSenderId: '362779009246',
+    measurementId: 'G-4W6VWW2FPD',
+  },
   production: true
 };
