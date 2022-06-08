1601d795ddbadf6c9ee737f83a31bda3133e8c21
Sander Jochems <sanderjochems@hotmail.nl>
2022-06-08 13:53:53 +0200

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,9 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Huishoudboekje'`, () => {
+  it(`should have as title 'Checkbook'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Huishoudboekje');
+    expect(app.title).toEqual('Checkbook');
   });
 });
