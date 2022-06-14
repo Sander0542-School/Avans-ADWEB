2d987f38a41e73d8ad545a0adf3e39b450dd905c
Sander Jochems <sanderjochems@hotmail.nl>
2022-06-14 16:44:24 +0200

    Create Checkbook Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {canActivate, redirectUnauthorizedTo} from '@angular/fire/auth-guard';
 import {HomeComponent} from "./pages/home/home.component";
 import {CheckbookListComponent} from "./pages/checkbook/checkbook-list/checkbook-list.component";
+import {CheckbookComponent} from "./pages/checkbook/checkbook/checkbook.component";
 
 const requireLogin = canActivate(() => redirectUnauthorizedTo(['']));
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'checkbooks', component: CheckbookListComponent, ...requireLogin},
+  {path: 'checkbooks/:id', component: CheckbookComponent, ...requireLogin},
 ];
 
 @NgModule({
