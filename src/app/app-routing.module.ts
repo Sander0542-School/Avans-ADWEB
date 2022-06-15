6df3c606002886db9b2a2731b0867e0a110915d7
Sander Jochems <sanderjochems@hotmail.nl>
2022-06-15 11:21:56 +0200

    Create checkbook-archived component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {canActivate, redirectUnauthorizedTo} from '@angular/fire/auth-guard';
 import {HomeComponent} from "./pages/home/home.component";
 import {CheckbookListComponent} from "./pages/checkbook/checkbook-list/checkbook-list.component";
 import {CheckbookComponent} from "./pages/checkbook/checkbook/checkbook.component";
+import {CheckbookArchivedComponent} from "./pages/checkbook/checkbook-archived/checkbook-archived.component";
 
 const requireLogin = canActivate(() => redirectUnauthorizedTo(['']));
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'checkbooks', component: CheckbookListComponent, ...requireLogin},
+  {path: 'checkbooks/archived', component: CheckbookArchivedComponent, ...requireLogin},
   {path: 'checkbooks/:id', component: CheckbookComponent, ...requireLogin},
 ];
 
