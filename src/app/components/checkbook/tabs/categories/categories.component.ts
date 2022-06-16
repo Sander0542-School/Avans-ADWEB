ca148640384f3fdf5230ad1d66948407b6562c79
Sander Jochems <sanderjochems@hotmail.nl>
2022-06-16 15:31:34 +0200

    Update CategoriesComponent

--- src/app/components/checkbook/tabs/categories/categories.component.ts
+++ src/app/components/checkbook/tabs/categories/categories.component.ts
@@ -1,19 +1,31 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Checkbook} from "../../../../models/checkbook";
+import {Category} from "../../../../models/category";
+import {MatDialog} from "@angular/material/dialog";
+import {CategoryService} from "../../../../services/category.service";
+import {CategoryDialogComponent} from "../../categories/dialogs/category-dialog/category-dialog.component";
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss']
 })
-export class CategoriesComponent implements OnInit {
+export class CategoriesComponent {
 
   @Input()
   public checkbook: Checkbook = {} as Checkbook;
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog,
+    private categoryService: CategoryService
+  ) {
+  }
 
-  ngOnInit(): void {
+  openCategoryDialog(category?: Category) {
+  }
+
+  async deleteCategory(category: Category) {
+    await this.categoryService.deleteCategory(this.checkbook, category);
   }
 
 }
