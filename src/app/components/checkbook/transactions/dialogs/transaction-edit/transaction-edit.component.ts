7d966a220e36e497ccb14b17c272fe14e8e796d2
Tommy den Reijer <30556476+tommydr@users.noreply.github.com>
2022-06-16 11:28:20 +0200

    removed unused code

--- src/app/components/checkbook/transactions/dialogs/transaction-edit/transaction-edit.component.ts
+++ src/app/components/checkbook/transactions/dialogs/transaction-edit/transaction-edit.component.ts
@@ -1,10 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {Checkbook} from "../../../../../models/checkbook";
-import {CheckbookService} from "../../../../../services/checkbook.service";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {AuthService} from "../../../../../services/auth.service";
-import {Transaction} from "../../../../../models/transaction";
 import {TransactionService} from "../../../../../services/transaction.service";
 
 @Component({
@@ -20,14 +16,12 @@ export class TransactionEditComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public data: any,
     private transactionService: TransactionService,
     private formBuilder: FormBuilder,
-    private authService: AuthService,
   ) {
     this.editPageForm = this.formBuilder.group({
       value: ['', Validators.required],
 
     });
     this.editPageForm.patchValue(this.data.transaction);
-
   }
 
   ngOnInit(): void {
@@ -35,7 +29,6 @@ export class TransactionEditComponent implements OnInit {
 
   async save() {
     if (!this.editPageForm.valid) return;
-
 
     const change = {
       value: this.editPageForm.value.value,
