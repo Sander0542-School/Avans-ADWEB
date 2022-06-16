ddc00c92fbdcd5d17ca0cf633cc416f0198c1c7a
Tommy den Reijer <30556476+tommydr@users.noreply.github.com>
2022-06-16 15:19:50 +0200

    removed tests

--- src/app/components/checkbook/tabs/transactions/transactions.component.spec.ts
+++ src/app/components/checkbook/tabs/transactions/transactions.component.spec.ts
@@ -19,7 +19,7 @@ describe('TransactionsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
 });
