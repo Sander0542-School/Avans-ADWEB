1a8cfc9e9d958da2bbdfe1139fb60522f07983fd
Sander Jochems <sanderjochems@hotmail.nl>
2022-06-17 10:10:54 +0200

    Update categories.component

--- src/app/components/checkbook/tabs/categories/categories.component.ts
+++ src/app/components/checkbook/tabs/categories/categories.component.ts
@@ -4,8 +4,7 @@ import {CategoryService} from "../../../../services/category.service";
 import {Category} from "../../../../models/category";
 import {MatDialog} from "@angular/material/dialog";
 import {CategoryDialogComponent} from "../../categories/dialogs/category-dialog/category-dialog.component";
-import {TableAction} from "../../categories/category-list/category-list.component";
-import {BehaviorSubject, Observable, switchMap} from "rxjs";
+import {Observable, switchMap} from "rxjs";
 
 @Component({
   selector: 'app-categories',
@@ -19,54 +18,27 @@ export class CategoriesComponent implements OnInit {
   private checkbookCache!: Checkbook;
 
   public categories!: Observable<Category[]>;
-  public selectedCategory: BehaviorSubject<Category | undefined>
-
-  public listActions: TableAction[] = [
-    {
-      name: 'Edit',
-      action: (category: Category) => this.openCategoryDialog(category),
-    },
-    {
-      name: 'Delete',
-      action: (category: Category) => this.deleteCategory(category),
-    },
-    {
-      name: 'Select',
-      action: (category: Category) => this.selectCategory(category),
-    }
-  ]
 
   constructor(
     public dialog: MatDialog,
     private categoryService: CategoryService
   ) {
-    this.selectedCategory = new BehaviorSubject<Category | undefined>(undefined);
   }
 
   ngOnInit(): void {
     this.categories = this.checkbook.pipe(
       switchMap(checkbook => {
         this.checkbookCache = checkbook;
-        this.selectedCategory.next(undefined);
         return this.categoryService.getCategories(checkbook);
       })
     )
   }
 
-  selectCategory(category: Category) {
-    this.selectedCategory.next(category);
-  }
-
-  openCategoryDialog(category?: Category) {
+  createCategory() {
     this.dialog.open(CategoryDialogComponent, {
       data: {
-        checkbook: this.checkbookCache,
-        category: category
-      },
+        checkbook: this.checkbookCache
+      }
     });
-  }
-
-  async deleteCategory(category: Category) {
-    await this.categoryService.deleteCategory(this.checkbookCache, category);
   }
 }
